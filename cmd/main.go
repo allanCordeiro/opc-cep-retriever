f1101142592b93152fba50ef3ec3a9ee30d6a7cb
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2022-12-19 23:13:26 -0300

    feat: add prometheus metrics endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"cep-retriever/internal/infra/webserver"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
@@ -17,7 +18,7 @@ import (
 
 // @contact.name Allan Cordeiro
 // @contact.url  http://www.allancordeiro.com
-// @contat.email eu@allancordeiro.com
+// @contact.email eu@allancordeiro.com
 
 // @host     localhost:8080
 // @basePath /
@@ -25,10 +26,10 @@ func main() {
 	log.Println("Server running")
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Handle("/metrics", promhttp.Handler())
 	r.Route("/retrieve", func(r chi.Router) {
 		r.Get("/{cep}", webserver.CepHandleGet)
 	})
 	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
-
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
